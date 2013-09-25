bf2264a56c7a136baa0e3baa51fad7ffec296e40
Iskander Haziev <gvalmon@gmail.com>
2013-09-25 22:04:48 +0400

    make decorable? method public

--- lib/cyrax/extensions/has_decorator.rb
+++ lib/cyrax/extensions/has_decorator.rb
@@ -6,20 +6,18 @@ module Cyrax::Extensions
       class_attribute :decorator_class_name
     end
 
-    module ClassMethods
-      def decorator(name)
-        self.decorator_class_name = name ? name.to_s : nil
-      end
-    end
-
-    private
-
     def decorable?
       !self.class.decorator_class_name.nil?
     end
 
     def decorator_class
       self.class.decorator_class_name.to_s.classify.constantize
+    end
+
+    module ClassMethods
+      def decorator(name)
+        self.decorator_class_name = name ? name.to_s : nil
+      end
     end
   end
 end
