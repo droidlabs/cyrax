7b16598863397146b97204fa944c11ebd817da30
Iskander Haziev <gvalmon@gmail.com>
2014-03-18 15:22:18 +0400

    added total count support to serializer

--- lib/cyrax/serializer.rb
+++ lib/cyrax/serializer.rb
@@ -1,12 +1,35 @@
 class Cyrax::Serializer < Cyrax::Wrapper
   def serialize
     options[:serializer] = self
+    
+    if block = self.class.total_count_block
+      serialize_wrapped(options, &block)
+    else
+      serialize_simple(options)
+    end
+  end
+
+  def serialize_simple(options = {})
     self.class.scope.serialize(resource, options)
+  end
+
+  def serialize_wrapped(options = {}, &counter_block)
+    total_count = self.instance_eval(&counter_block)
+    data = self.class.scope.serialize(resource, options)
+    {total_count: total_count, data: data}
   end
 
   class << self
     def scope
       @scope ||= Cyrax::Serializers::Scope.new()
+    end
+
+    def total_count(&block)
+      @total_count_block = block
+    end
+
+    def total_count_block
+      @total_count_block
     end
 
     def namespace(name, &block)
