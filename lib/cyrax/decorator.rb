fd9f16ec5e397dc764ddfcfb3c1ae3cb1bd0843b
Timur Valeev <kibenimatik@yandex.ru>
2014-08-08 11:25:22 +0400

    Cyrax::Decorator: add class methods #respond_to and #method_missing

--- lib/cyrax/decorator.rb
+++ lib/cyrax/decorator.rb
@@ -21,11 +21,28 @@ class Cyrax::Decorator < Cyrax::Wrapper
     resource.send(method, *args, &block)
   end
 
+  def respond_to?(method_sym, include_private = false)
+    super || resource.respond_to?(method_sym, include_private)
+  end
+
   class << self
     alias_method :decorate, :new
 
     def decorate_collection(resource)
       Cyrax::Presenters::DecoratedCollection.decorate(resource, decorator: self)
+    end
+
+    def respond_to?(method_sym, include_private = false)
+      super || @@decorated_class.respond_to?(method_sym, include_private)
+    end
+
+    def method_missing(method, *args, &block)
+      return super unless @@decorated_class.respond_to?(method)
+      @@decorated_class.send(method, *args, &block)
+    end
+
+    def decorated_class(klass)
+      @@decorated_class = klass
     end
   end
 end
