44313565f233f7c606866b9368eb0cd3750221f7
Iskander Haziev <gvalmon@gmail.com>
2015-09-09 12:25:03 +0300

    handle empty relation correctly

--- spec/cyrax/serializer_spec.rb
+++ spec/cyrax/serializer_spec.rb
@@ -8,16 +8,25 @@ class FooSerializer < Cyrax::Serializer
   relation :another do
     attributes :another_foo, :another_bar
   end
+  relation :empty_relation_many do
+    attributes :another_foo, :another_bar
+  end
+  relation :empty_relation_one do
+    attributes :another_foo, :another_bar
+  end
 end
 
 module Cyrax
   describe FooSerializer do
-    let(:serializable) { 
+    let(:serializable) {
       double(
         name: 'me',
         foo: '2342',
         bar: '4223',
-        another: double(another_foo: '1234', another_bar: '1234')) 
+        another: double(another_foo: '1234', another_bar: '1234'),
+        empty_relation_one: nil,
+        empty_relation_many: []
+      )
     }
     subject { FooSerializer.new(serializable).serialize }
 
@@ -25,6 +34,8 @@ module Cyrax
       subject[:name].should eq('me')
       subject[:some][:foo].should eq('2342')
       subject[:another][:another_foo].should eq('1234')
+      subject[:empty_relation_one].should eq(nil)
+      subject[:empty_relation_many].should eq([])
     end
   end
 end
